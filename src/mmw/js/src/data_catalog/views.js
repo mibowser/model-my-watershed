5f737c9b70cf6d4ddc234ac527b344f65b035d2b
Alice Rottersman <arottersman@azavea.com>
2017-08-01 09:32:16 -0400

    Fix BiGCZ Tabs On IE11
    - previous: Switching tabs in BiGCZ wouldn't change the tab contents. IE you'd see CINERGI results on all three tabs
    - now: fixed. Can't set backbone `className` dynamically. IE11 only executed the function that returned className once. Now we set the active class with jQuery

--- src/mmw/js/src/data_catalog/views.js
+++ src/mmw/js/src/data_catalog/views.js
@@ -236,9 +236,7 @@ var ErrorView = Marionette.ItemView.extend({
 });
 
 var TabContentView = Marionette.LayoutView.extend({
-    className: function() {
-        return 'tab-pane' + (this.model.get('active') ? ' active' : '');
-    },
+    className: 'tab-pane',
     id: function() {
         return this.model.id;
     },
@@ -258,10 +256,13 @@ var TabContentView = Marionette.LayoutView.extend({
     },
 
     modelEvents: {
-        'change': 'update'
+        'change': 'update',
+        'change:active': 'toggleActiveClass',
     },
 
     onShow: function() {
+        this.toggleActiveClass();
+
         this.resultRegion.show(new ResultsView({
             collection: this.model.get('results'),
             catalog: this.model.id,
@@ -277,6 +278,10 @@ var TabContentView = Marionette.LayoutView.extend({
                 model: this.model,
             }));
         }
+    },
+
+    toggleActiveClass: function() {
+        this.$el.toggleClass('active', this.model.get('active'));
     },
 
     update: function() {
